cc6c71961a9dc9cef52e50e8dc8a5eb989367b34
Mourya Prasanth <mouryaprasanthp@gmail.com>
2022-05-27 06:42:14 +0530

    fix image and priority updating issue

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import DatePicker from "react-datepicker";
 import Select from 'react-select'
 import { v4 as uuidv4 } from 'uuid';
+import { FcAddImage } from 'react-icons/fc';
 import "react-datepicker/dist/react-datepicker.css";
 import tags from '../tags.json';
 
@@ -12,9 +13,7 @@ const TaskForm = ({ modal, toggle, onCreate, task: taskData, defaultTask, type =
   const handleChange = (e) => {
     const { name, value } = e.target;
 
-    if (!["tags", "image", "deadline"].includes(name)) {
       setTask(prev => ({ ...prev, [name]: value }))
-    }
   }
   const handlePriority = (e) => {
     const { checked } = e.target;
@@ -50,14 +49,15 @@ const TaskForm = ({ modal, toggle, onCreate, task: taskData, defaultTask, type =
             <div className='modal-body'>
               <div className='modal-form'>
                 <div className="form-group" >
-                  <label htmlFor="title">
+                  <label htmlFor={`title-${task.id}`}>
                     Task
                   </label>
-                  <input type="text" name="title" value={task.title} onChange={handleChange} />
+                  <input id={`title-${task.id}`} type="text" name="title" value={task.title} onChange={handleChange} />
                 </div>
                 <div className="form-group" >
-                  <label htmlFor="deadline">Deadline</label>
+                  <label htmlFor={`deadline-${task.id}`}>Deadline</label>
                   <DatePicker
+                    id={`deadline-${task.id}`}
                     selected={task.deadline ? new Date(task.deadline) : null}
                     onChange={(date) => setTask(prev => ({ ...prev, deadline: date }))}
                     showTimeSelect
@@ -67,23 +67,26 @@ const TaskForm = ({ modal, toggle, onCreate, task: taskData, defaultTask, type =
                   />
                 </div>
                 <div className="form-group" >
-                  <label htmlFor="description">
+                  <label htmlFor={`description-${task.id}`}>
                     Description
                   </label>
-                  <textarea name="description" value={task.description} onChange={handleChange} />
+                  <textarea
+                    id={`description-${task.id}`}
+                    name="description" value={task.description} onChange={handleChange} />
                 </div>
                 <div className="form-group priority-input-container">
-                  <label htmlFor="priority">
-                    <input type="checkbox" id="priority" name="priority" onChange={handlePriority} checked={task.priority} /> Priority
+                  <label htmlFor={`priority-${task.id}`}>
+                    <input type="checkbox" id={`priority-${task.id}`} name="priority" onChange={handlePriority} checked={task.priority} /> Priority
                   </label>
                 </div>
 
                 <div className='form-group'>
-                  <label htmlFor="tags">
+                  <label htmlFor={`tags-${task.id}`}>
                     Tags
                   </label>
                   <div>
                     <Select
+                      id={`tags-${task.id}`}
                       options={tags.map(tag => ({ value: tag.name, label: tag.name, ...tag }))}
                       isMulti
                       onChange={(selectedOption) => setTask(prev => ({ ...prev, tags: selectedOption.map(({ id, name }) => ({ id, name })) }))}
@@ -95,11 +98,12 @@ const TaskForm = ({ modal, toggle, onCreate, task: taskData, defaultTask, type =
                 </div>
                 <div className="form-group" >
                   Image
-                  <label htmlFor="image">
-                    <img src={task.image} alt="" width="200" style={{ cursor: 'pointer' }} />
+                  {console.log({ title: task.title, task })}
+                  <label htmlFor={`image-${task.id}`} style={{ cursor: 'pointer' }}>
+                    {task.image.length > 0 ? <img src={task.image} alt="" width="200" /> : <FcAddImage fontSize={50} />}
                   </label>
                   <input
-                    id="image"
+                    id={`image-${task.id}`}
                     className='input-image'
                     name="image"
                     accept="image/*"
